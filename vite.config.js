6072c689505ecd65db4a30bb3cbc36b821fdac01
Murali Krishna Mididoddi <112509268+muraliKrishnaMididoddi@users.noreply.github.com>
2025-07-30 22:21:12 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
+   base: '/Murali_portfolio.github.io/',
   plugins: [react(), tailwindcss()],
 });
