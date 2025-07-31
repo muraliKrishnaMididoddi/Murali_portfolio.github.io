8e16097d01d39685491930857c553a895a80ec2a
Murali Krishna Mididoddi <112509268+muraliKrishnaMididoddi@users.noreply.github.com>
2025-07-31 01:14:36 -0400

    Update HeroText.jsx

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -2,7 +2,7 @@ import { FlipWords } from "./FlipWords";
 import { motion } from "motion/react";
 
 const HeroText = () => {
-  const words = ["Secure", "Modern", "Scalable"];
+  const words = ["Intelligent", "	Insightful", "Predictive","Actionable", "Scalable"];
   const variants = {
     hidden: { opacity: 0, x: -50 },
     visible: { opacity: 1, x: 0 },
@@ -18,7 +18,7 @@ const HeroText = () => {
           animate="visible"
           transition={{ delay: 1 }}
         >
-          Hi I'm Ali
+          Hi I'm Murali
         </motion.h1>
         <div className="flex flex-col items-start">
           <motion.p
@@ -28,7 +28,7 @@ const HeroText = () => {
             animate="visible"
             transition={{ delay: 1.2 }}
           >
-            A Developer <br /> Dedicated to Crafting
+            A Data Professional <br /> Dedicated to Crafting
           </motion.p>
           <motion.div
             variants={variants}
@@ -48,7 +48,7 @@ const HeroText = () => {
             animate="visible"
             transition={{ delay: 1.8 }}
           >
-            Web Solutions
+            Data Solutions
           </motion.p>
         </div>
       </div>
