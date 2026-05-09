5792e11209ebf6c2b638878c26fc62a3e4e5a9f7
Murali Krishna Mididoddi <112509268+muraliKrishnaMididoddi@users.noreply.github.com>
2026-05-08 21:28:03 -0400

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
-export default defineConfig({
-   base: '/Murali_portfolio.github.io/',
-  plugins: [react(), tailwindcss()],
-});
